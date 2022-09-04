8f4c69a0d69e3577ec267ecf4da63a6ce38f677d
JDFSHU <jacob.d.frazer@googlemail.com>
2022-09-04 15:56:28 +0100

    modified

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ while [ $exit_loop -ne 1 ]
 do
     echo "1 - Install $ipython"
     echo "2 - Install $ipip"
-    echo "3 - Install $iat (job schedueler"
+    echo "3 - Install $iat"
     echo "4 - install $inet"
     echo "5 - install $itree"
     echo "6 - install $iranger"
@@ -32,33 +32,37 @@ do
 
     read choice;
     case $choice in 
-    1)  
-    if command -v $ipython3
-    then
-        echo "$ipython already installed"
-    else
-        echo "Installing $ipython"
-        sudo apt install -y python3 >> $install_log
-        error_function
-        echo "Installed $ipython on $(date)." >> $install_log
-    fi;;
 
-    2) echo "Installing $ipip";;
-    3) 
+        1)
+        if command -v $ipython3
+        then
+            echo "$ipython already installed"
+        else
+            echo "Installing $ipython"
+            sudo apt install -y python3 >> $install_log 2>> $error_log
+            error_function
+            echo "Installed $ipython on $(date)." >> $install_log
+        fi
+        ;;
+
+        2) echo "Installing $ipip";;
+        3) 
         if command -v $iat
         then
             echo "'$iat' already installed"
         else
             echo "Installing '$iat'"
-            sudo apt install -y $iat
+            sudo apt install -y $iat >> $install_log 2>> $error_log
             error_function
             echo "Installed '$iat' on $(date)." 
-        fi;;
-    4) echo "Installing $inet";;
-    5) echo "Installing $itree";;
-    6) echo "Installing $iranger";;
-    7) echo "Installing $ihtop";;
-    8) exit_loop=1 ;;
-    *) echo "Incorrect Input";;
+        fi
+        ;;
+
+        4) echo "Installing $inet";;
+        5) echo "Installing $itree";;
+        6) echo "Installing $iranger";;
+        7) echo "Installing $ihtop";;
+        8) exit_loop=1 ;;
+        *) echo "Incorrect Input";;
     esac
 done
