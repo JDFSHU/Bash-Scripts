9c0b65f10f568020146a89d6c36e28c796a7ee65
JDFSHU <jacob.d.frazer@googlemail.com>
2022-09-05 19:10:43 +0100

    added functions instead of repeated IF statement

--- jfinstaller.sh
+++ jfinstaller.sh
@@ -4,25 +4,22 @@ exit_loop=0 # used to break the while loop
 
 # ipackage(installPackage) variables, add additional variables for additional packages you want the installer to take care of
 ipython=python3
-command_pip=pip3 # pip3 install and command are different, hence two variables
 ipip=python3-pip
 iat=at
 itree=tree
-command_net=ifconfig
 inet=net-tools
 iranger=ranger
 ihtop=htop
+
+# functions check for commands, some commands aren't identical to the installation name for the packages, hence the extra variables here which will be passed into a seperate function
+command_pip=pip3
+command_net=ifconfig
+
+# Log files saved to /var/log
 error_log=/var/log/jfinstaller_errors.log
 install_log=/var/log/jfinstaller.log
 
-error_function() {
-    if [ $? -ne 0 ]
-    then
-        echo -e "An error has occured, please check the $error_log file\n"
-        sleep 5
-    fi
-}
-
+# Pretty menu print out, just testing colouration of echo output 
 menu_function() {
 echo -e '\e[0;32m        _   ______   _                 _             _   _               \e[0m'
 echo -e '\e[0;32m       | | |  ____| (_)               | |           | | | |             \e[0m'
@@ -33,11 +30,52 @@ echo -e '\e[0;32m   \____/  |_|      |_| |_| |_| |___/  \__|  \__,_| |_| |_|  \_
 echo -e "\n"
 }
 
+# This function checks if a command is already available, if it is it prints that its already installed, if not it installs the relevant package
+case_function_one_arg() {
+    if command -v $1
+    then
+        echo -e "\n'$1' already installed\n"
+        sleep_function
+    else
+        echo -e "\nInstalling '$1'\n"
+        sudo apt-get install -y $1 >> $install_log 2>> $error_log
+        error_function
+        echo -e "'$1' installation complete\n"
+        echo -e "Installed '$1' on $(date).\n" >> $install_log
+        sleep_function
+    fi
+}
+
+# This second function does the same but uses two arguments incase of differening commands vs package name: pip3 vs python3-pip
+case_function_two_arg() {
+    if command -v $1
+    then
+        echo -e "\n'$2' already installed\n"
+        sleep_function
+    else
+        echo -e "\nInstalling '$2'\n"
+        sudo apt-get install -y $2 >> $install_log 2>> $error_log
+        error_function
+        echo -e "'$2' installation complete\n"
+        echo -e "Installed '$2' on $(date).\n" >> $install_log
+        sleep_function
+    fi
+}
+
+# if $? exit code isn't 0(successful) tells the user to check the error log file
+error_function() {
+    if [ $? -ne 0 ]
+    then
+        echo -e "An error has occured, please check the $error_log file\n"
+        sleep 5
+    fi
+}
+
+# This just pauses the terminal for 1.5 seconds before clearing the screen to create a smooth browsing experience
 sleep_function() {
     sleep 1.5
     clear
 }
-
 
 while [ $exit_loop -ne 1 ]
 do # extra echo statements can be added here for additional package installations, just increment the number choices and change the exit number e.g 8 = install x 9 = exit
@@ -51,123 +89,23 @@ do # extra echo statements can be added here for additional package installation
     echo "6 - install $iranger"
     echo "7 - install $ihtop"
     echo "8 - Exit"
-    
 
     read choice; # reading input from the user
     case $choice in # case statements to install packages or advice already installed
-
-        1)
-        if command -v $ipython
-        then
-            echo -e "\n'$ipython' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$ipython'\n"
-            sudo apt-get install -y $ipython >> $install_log 2>> $error_log
-            error_function
-            echo -e "\n'$ipython' installation complete\n"
-            echo -e "Installed '$ipython' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        2)
-        if command -v $command_pip
-        then
-            echo -e "\n'$command_pip' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$command_pip'\n"
-            sudo apt-get install -y $ipip >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$command_pip' installation complete\n"
-            echo -e "Installed '$command_pip' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        3) 
-        if command -v $iat
-        then
-            echo -e "\n'$iat' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$iat'\n"
-            sudo apt-get install -y $iat >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$iat' installation complete\n"
-            echo -e "Installed '$iat' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        4) 
-        if command -v $command_net
-        then
-            echo -e "\n'$inet' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$inet'\n"
-            sudo apt-get install -y $inet >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$inet' installation complete\n"
-            echo -e "Installed '$inet' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        5)
-        if command -v $itree
-        then
-            echo -e "\n'$itree' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$itree'\n"
-            sudo apt-get install -y $itree >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$itree' installation complete\n"
-            echo -e "Installed '$itree' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        6)
-        if command -v $iranger
-        then
-            echo -e "\n'$iranger' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$iranger'\n"
-            sudo apt-get install -y $iranger >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$iranger' installation complete\n"
-            echo -e "Installed '$iranger' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-
-        7)
-        if command -v $ihtop
-        then
-            echo -e "\n'$ihtop' already installed\n"
-            sleep_function
-        else
-            echo -e "\nInstalling '$ihtop'\n"
-            sudo apt-get install -y $ihtop >> $install_log 2>> $error_log
-            error_function
-            echo -e "'$ihtop' installation complete\n"
-            echo -e "Installed '$ihtop' on $(date).\n" >> $install_log
-            sleep_function
-        fi
-        ;;
-        # any extra package installations can be added here, simply copy on of the case statements and create and utilise an ipackage variable
-        8) exit_loop=1 ;;
-
-        *) 
+        
+        1) case_function_one_arg $ipython;; # calling the one argument case function
+        2) case_function_two_arg $command_pip $ipip;; # two argument case function, use this if the command checked for isn't the same as the package name IE pip3 vs python3-pip
+        3) case_function_one_arg $iat;;
+        4) case_function_two_arg $command_net $inet;;
+        5) case_function_one_arg $itree;;
+        6) case_function_one_arg $iranger;;
+        7) case_function_one_arg $ihtop;;
+        8) exit_loop=1;;
+        *)
         echo -e "\nIncorrect Input\n"
         sleep_function
-    esac # closing cases
-done # end of while loop
+    esac
+done
 
 clear # clears terminal
 echo -e "\nThank you for using JFinstaller\n"
